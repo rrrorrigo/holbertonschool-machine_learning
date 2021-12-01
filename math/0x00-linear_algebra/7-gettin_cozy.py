5eee51a67c94b51fb097ad510a80097e8e6b1166
rrrorrigo <rodrigobriones54@gmail.com>
2021-12-01 17:18:39 -0300

    added border case

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -10,7 +10,7 @@ def cat_matrices2D(mat1, mat2, axis=0):
         for i in range(len(mat2)):
             mat1Copy.append(mat2Copy[i])
         return mat1Copy
-    if axis == 1 and len(mat1[0]) == len(mat2[0]):
+    if axis == 1 and len(mat1) == len(mat2):
         newMat = []
         for i in range(len(mat1)):
             newMat.append(mat1Copy[i] + mat2Copy[i])
