3ba1ada2ccc74d9d017e9e250eacaea5a8444864
rrrorrigo <rodrigobriones54@gmail.com>
2021-12-01 17:15:21 -0300

    added task

---
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""cat_matrices2D function"""
+
+
+def cat_matrices2D(mat1, mat2, axis=0):
+    """ function that concatenates two matrices along a specific axis"""
+    mat1Copy = [row[:] for row in mat1]
+    mat2Copy = [row[:] for row in mat2]
+    if axis == 0:
+        for i in range(len(mat2)):
+            mat1Copy.append(mat2Copy[i])
+        return mat1Copy
+    if axis == 1:
+        newMat = []
+        for i in range(len(mat1)):
+            newMat.append(mat1Copy[i] + mat2Copy[i])
+        return newMat
+    else:
+        return None
