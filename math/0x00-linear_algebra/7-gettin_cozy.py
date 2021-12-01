d3595bbf5d694ac8a70c9b2cc09216f1745138b1
rrrorrigo <rodrigobriones54@gmail.com>
2021-12-01 17:16:44 -0300

    added border case

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -6,11 +6,11 @@ def cat_matrices2D(mat1, mat2, axis=0):
     """ function that concatenates two matrices along a specific axis"""
     mat1Copy = [row[:] for row in mat1]
     mat2Copy = [row[:] for row in mat2]
-    if axis == 0:
+    if axis == 0 and len(mat1[0]) == len(mat2[0]):
         for i in range(len(mat2)):
             mat1Copy.append(mat2Copy[i])
         return mat1Copy
-    if axis == 1:
+    if axis == 1 and len(mat1[0]) == len(mat2[0]):
         newMat = []
         for i in range(len(mat1)):
             newMat.append(mat1Copy[i] + mat2Copy[i])
