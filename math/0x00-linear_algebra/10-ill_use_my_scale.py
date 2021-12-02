106c578469b972411b572b8e7ce5913174f8de5b
rrrorrigo <rodrigobriones54@gmail.com>
2021-12-01 23:06:37 -0300

    added task 10

---
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""I’ll Use My Scale"""
+import numpy as np
+
+
+def np_shape(matrix):
+    """function that calculates the shape of a numpy.ndarray"""
+    return np.shape(matrix)
