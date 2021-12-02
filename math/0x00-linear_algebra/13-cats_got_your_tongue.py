705a60704834dce134de03697a57f2a9dbb6eaab
rrrorrigo <rodrigobriones54@gmail.com>
2021-12-01 23:37:12 -0300

    added task 13

---
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Cat's Got Your Tongue"""
+
+
+def np_cat(mat1, mat2, axis=0):
+    """function that concatenates two matrices along a specific axis"""
+    import numpy as np
+    return np.concatenate((mat1, mat2), axis=axis)
