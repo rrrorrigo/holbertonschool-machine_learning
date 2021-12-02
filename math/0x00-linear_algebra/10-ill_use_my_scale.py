5b7967e0bb4f466571dfbd8436905125c6682600
rrrorrigo <rodrigobriones54@gmail.com>
2021-12-01 23:11:58 -0300

    checker i hate you

--- math/0x00-linear_algebra/10-ill_use_my_scale.py
+++ math/0x00-linear_algebra/10-ill_use_my_scale.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 """I’ll Use My Scale"""
-import numpy as np
 
 
 def np_shape(matrix):
     """function that calculates the shape of a numpy.ndarray"""
-    return np.shape(matrix)
+    return matrix.shape
