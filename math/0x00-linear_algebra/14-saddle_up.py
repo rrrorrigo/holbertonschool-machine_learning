47db31e42ac1317c23cfc0055e52401c2f035df6
rrrorrigo <rodrigobriones54@gmail.com>
2021-12-01 23:43:07 -0300

    added task 14

---
+++ math/0x00-linear_algebra/14-saddle_up.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""Saddle Up"""
+
+
+import numpy as np
+
+
+def np_matmul(mat1, mat2):
+    """function that performs matrix multiplication"""
+    return np.matmul(mat1, mat2)
