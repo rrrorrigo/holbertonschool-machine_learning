f3a58ef30db804740e51556b5907db4de27ea746
rrrorrigo <rodrigobriones54@gmail.com>
2021-12-01 23:38:05 -0300

    checker i hate you

--- math/0x00-linear_algebra/13-cats_got_your_tongue.py
+++ math/0x00-linear_algebra/13-cats_got_your_tongue.py
@@ -2,6 +2,9 @@
 """Cat's Got Your Tongue"""
 
 
+import numpy as np
+
+
 def np_cat(mat1, mat2, axis=0):
     """function that concatenates two matrices along a specific axis"""
     import numpy as np
