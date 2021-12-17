877609219279c94c51e34512d1da0528b330f6f3
rrrorrigo <rodrigobriones54@gmail.com>
2021-12-17 14:50:02 -0300

    added task 1

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -11,6 +11,8 @@ class Poisson:
         lambtha: is the expected number of occurences in a given time frame
         """
         self.lambtha = float(lambtha)
+        self.π = 3.1415926536
+        self.e = 2.7182818285
 
         if data is None:
             if lambtha <= 0:
@@ -21,3 +23,18 @@ class Poisson:
             elif len(data) < 2:
                 raise ValueError("data must contain multiple values")
             self.lambtha = float(sum(data) / len(data))
+
+    def factorial(self, n):
+        """Method that return the factorial result of n"""
+        fac = 1
+        for number in range(1, n + 1):
+            fac = fac * number
+        return fac
+
+    def pmf(self, k):
+        """Calculates the value of the PMF for a given number of “successes”"""
+        if type(k) is not int:
+            k = int(k)
+        if k <= 0:
+            return 0
+        return self.lambtha**k * self.e**-self.lambtha / self.factorial(k)
