38e4aaf5ee2f4673e6177fcf298bea0c9ee44375
rrrorrigo <rodrigobriones54@gmail.com>
2021-12-17 17:48:10 -0300

    added task 3

---
+++ math/0x03-probability/exponential.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""3. Initialize Exponential"""
+
+
+class Exponential:
+    """class Exponential that represents an exponential distribution"""
+    def __init__(self, data=None, lambtha=1.):
+        """Class contructor
+        
+        data: is a list of the data to be used to estimate the distribution
+        lambtha: is the expected number of occurences in a given time frame
+        """
+        self.lambtha = float(lambtha)
+
+        if data is None:
+            if lambtha < 0:
+                raise ValueError("lambtha must be a positive value")
+        if type(data) is not list:
+            raise TypeError("data must be a list")
+        if len(data) < 2:
+            raise ValueError("data must contain multiple values")
+        self.lambtha = sum(data) / len(data)
