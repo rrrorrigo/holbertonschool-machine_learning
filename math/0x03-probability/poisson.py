3c12534c6e8531a1fa4077f145ee58e9368d2f69
rrrorrigo <rodrigobriones54@gmail.com>
2021-12-17 17:31:46 -0300

    added method to task 2

--- math/0x03-probability/poisson.py
+++ math/0x03-probability/poisson.py
@@ -38,3 +38,13 @@ class Poisson:
         if k <= 0:
             return 0
         return self.lambtha**k * self.e**-self.lambtha / self.factorial(k)
+
+    def cdf(self, k):
+        """Calculates the value of the CDF for a given number of “successes”"""
+        k = int(k)
+        if k <= 0:
+            return 0
+        cdf = 0
+        for i in range(k + 1):
+            cdf += self.pmf(i)
+        return cdf
