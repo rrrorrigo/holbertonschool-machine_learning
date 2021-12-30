5636fee09d58ad1cc9ed0818ddf4735b423043b5
rrrorrigo <rodrigobriones54@gmail.com>
2021-12-29 21:30:08 -0300

    added some task  and its main files

--- supervised_learning/0x01-classification/8-neural_network.py
+++ supervised_learning/0x01-classification/8-neural_network.py
@@ -29,7 +29,7 @@ class NeuralNetwork:
             raise TypeError('nodes must be an integer')
         if nodes < 1:
             raise ValueError('nodes must be a positive integer')
-        self.W1 = np.random.randn(1, nx)
+        self.W1 = np.random.randn(nodes, nx)
         self.b1 = np.zeros((nodes, 1))
         self.A1 = 0
         # output of each neuron
--- supervised_learning/0x01-classification/9-neural_network.py
+++ supervised_learning/0x01-classification/9-neural_network.py
@@ -29,7 +29,7 @@ class NeuralNetwork:
             raise TypeError('nodes must be an integer')
         if nodes < 1:
             raise ValueError('nodes must be a positive integer')
-        self.__W1 = np.random.randn(1, nx)
+        self.__W1 = np.random.randn(nodes, nx)
         self.__b1 = np.zeros((nodes, 1))
         self.__A1 = 0
         # output of each neuron
