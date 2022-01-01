0212ff5b9ccc13ce693ed53c3677abc74c5cb5bc
rrrorrigo <rodrigobriones54@gmail.com>
2022-01-01 18:36:03 -0300

    added another border case

--- supervised_learning/0x01-classification/17-deep_neural_network.py
+++ supervised_learning/0x01-classification/17-deep_neural_network.py
@@ -18,8 +18,7 @@ class DeepNeuralNetwork:
         if nx < 1:
             raise ValueError('nx must be a positive integer')
         arr = np.array(layers)
-        if type(layers) is not list or len(layers) == 0 or np.any(arr < 0)\
-            or np.any(arr is int):
+        if type(layers) is not list or len(layers) == 0 or np.any(arr < 0):
             raise TypeError('layers must be a list of positive integers')
         self.__L = len(layers)
         self.__cache = {}
