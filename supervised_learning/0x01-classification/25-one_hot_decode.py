b6ea19ce58735329ab5bb3a0b3bf8d408d1caf0b
rrrorrigo <rodrigobriones54@gmail.com>
2022-01-03 22:55:21 -0300

    added border case

--- supervised_learning/0x01-classification/25-one_hot_decode.py
+++ supervised_learning/0x01-classification/25-one_hot_decode.py
@@ -7,4 +7,6 @@ import numpy as np
 
 def one_hot_decode(one_hot):
     """converts a one-hot matrix into a vector of labels"""
+    if type(one_hot) is not np.ndarray or one_hot.ndim != 2:
+        return None
     return one_hot.argmax(0)
