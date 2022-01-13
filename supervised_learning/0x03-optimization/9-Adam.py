c0ab1b512bf110cbd3c1f31731c022bde197e6a2
rrrorrigo <rodrigobriones54@gmail.com>
2022-01-13 12:03:38 -0300

    added task 9

--- supervised_learning/0x03-optimization/9-Adam.py
+++ supervised_learning/0x03-optimization/9-Adam.py
@@ -22,6 +22,6 @@ def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
     Returns: the updated variable, the new first moment, and the new second
     moment, respectively"""
     v = (beta1 * v + (1 - beta1) * grad) / (1 - (beta1 ** t))
-    s = (beta2 * s + (1 - beta2) * grad**2) / (1 - (beta2 ** t))
+    s = (beta2 * s + (1 - beta2) * (grad**2)) / (1 - (beta2 ** t))
     var -= alpha * (v / (np.sqrt(s) + epsilon))
     return var, v, s
