6e056e3c352703041fdd80cb93c2bae69257eee4
rrrorrigo <rodrigobriones54@gmail.com>
2022-01-20 16:35:02 -0300

    fixed method

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -17,18 +17,16 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     lambtha: is the L2 regularization parameter
     L: is the number of layers of the network"""
     m = Y.shape[1]
-    for i in range(L - 1, 0, -1):
-        A = cache['A' + str(i + 1)]
-        a = cache['A' + str(i)]
-        
-        if i == L - 1:
+    for i in range(L, 0, -1):
+        A = cache['A' + str(i)]
+        a = cache['A' + str(i - 1)]
+        if i == L:
             dz = A - Y
-            W = weights['W' + str(i + 1)]
         else:
             x = 1 - (A * A)
             dz = np.matmul(W.T, dz) * x
-            W = weights['W' + str(i + 1)]
-        dw = np.matmul(a, dz.T) / m
+        W = weights['W' + str(i)]
+        dw = np.matmul(dz, a.T) / m + (lambtha / m * W)
         db = np.sum(dz, axis=1, keepdims=True) / m
-        weights['W' + str(i + 1)] = W - alpha * (dw.T + (lambtha / m * W))
-        weights['b' + str(i + 1)] = weights['b' + str(i + 1)] - alpha * db
+        weights['W' + str(i)] = W - alpha * dw
+        weights['b' + str(i)] = weights['b' + str(i)] - alpha * db
