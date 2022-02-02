475a4f62d8a6c18f287430ce8587d9a910e7a457
Rodrigo Briones <rbriones@zonamerica.com>
2022-02-02 15:01:29 -0300

    added task 0

--- supervised_learning/0x07-cnn/0-conv_forward.py
+++ supervised_learning/0x07-cnn/0-conv_forward.py
@@ -35,51 +35,34 @@ def conv_forward(A_prev, W, b, activation, padding="same", stride=(1, 1)):
 
     Returns: a numpy.ndarray containing the convolved images"""
     m = A_prev.shape[0]
-    h_prev = A_prev.shape[1]
-    w_prev = A_prev.shape[2]
-    c_prev = A_prev.shape[3]
+    imagesNum = np.arange(m)
+    ih = A_prev.shape[1]
+    iw = A_prev.shape[2]
+    chnn = A_prev.shape[3]
     kh = W.shape[0]
     kw = W.shape[1]
-    c_prev = W.shape[2]
-    c_new = W.shape[3]
-    image_num = np.arange(m)
-    sh = stride[0]
-    sw = stride[1]
-
-    if padding == 'valid':
-        ph = 0
-        pw = 0
-    elif padding == 'same':
-        # output size depends on filter size and must be equal to image size
-        # imposing constraints on padding for a given set of strides
-        ph = int(np.ceil(((sh * h_prev) - sh + kh - h_prev) / 2))
-        pw = int(np.ceil(((sw * w_prev) - sw + kw - w_prev) / 2))
-
+    kp = W.shape[2]
+    ko = W.shape[3]
     if padding == 'same':
-        # pad A_prev before convolution, padding always symmetric here
-        A_prev = np.pad(A_prev, pad_width=((0, 0), (ph, ph), (pw, pw), (0, 0)),
-                        mode='constant')
-
-    output = np.zeros(shape=(m,
-                             int((h_prev - kh + 2 * ph) / sh + 1),
-                             int((w_prev - kw + 2 * pw) / sw + 1),
-                             c_new))
-
-    for k in range(c_new):
-        for i in range(int((h_prev - kh + 2 * ph) / sh + 1)):
-            for j in range(int((w_prev - kw + 2 * pw) / sw + 1)):
-                output[
-                    image_num,
-                    i,
-                    j,
-                    k
-                ] = np.sum(
-                    A_prev[
-                        image_num,
-                        i * sh: i * sh + kh,
-                        j * sw: j * sw + kw
-                    ] * W[:, :, :, k],
-                    axis=(1, 2, 3)
-                ) + b[0, 0, 0, k]
-    output = activation(output)
+        ph = int((ih - 1) * stride[0] + kh - ih // 2 + 1)
+        pw = int((iw - 1) * stride[1] + kw - iw // 2 + 1)
+        A_prev = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)))
+    if padding == 'valid':
+        ph, pw = (0, 0)
+    if type(padding) is tuple:
+        ph = padding[0]
+        pw = padding[1]
+    ch = (ih + 2 * ph - kh) // stride[0] + 1
+    cw = (iw + 2 * pw - kw) // stride[1] + 1
+    output = np.zeros((m, ch, cw, ko))
+    for y in range(ch):
+        for x in range(cw):
+            for n in range(ko):
+                x0 = x * stride[0]
+                y0 = y * stride[1]
+                X = x0 + kh
+                Y = y0 + kw
+                output[imagesNum, x, y, n] = activation(np.sum(
+                    W[:, :, :, n] * A_prev[imagesNum, x0: X, y0: Y],
+                axis=(1, 2, 3)) + b[:, :, :, n])
     return output
