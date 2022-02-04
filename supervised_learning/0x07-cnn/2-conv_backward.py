49f9d58fc49cc94e5764caeb1a8d85d26c5df883
Rodrigo Briones <77698169+rrrorrigo@users.noreply.github.com>
2022-02-03 23:07:43 -0300

    added task 2

--- supervised_learning/0x07-cnn/2-conv_backward.py
+++ supervised_learning/0x07-cnn/2-conv_backward.py
@@ -49,7 +49,7 @@ def conv_backward(dZ, A_prev, W, b, padding="same", stride=(1, 1)):
     if padding == 'same':
         ph = int(np.ceil(((stride[0] * h_prev) - stride[0] + kh - h_prev) / 2))
         pw = int(np.ceil(((stride[1] * w_prev) - stride[1] + kw - w_prev) / 2))
-    A_prev = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)))
+        A_prev = np.pad(A_prev, ((0, 0), (ph, ph), (pw, pw), (0, 0)))
     output = np.zeros(shape=A_prev.shape)
     dW = np.zeros(shape=W.shape)
     db = np.zeros(shape=b.shape)
