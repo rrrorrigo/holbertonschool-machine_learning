a18d98c8c62e36a63bc6e788bd9eead79604d593
rrrorrigo <rodrigobriones54@gmail.com>
2022-02-10 20:02:46 -0300

    added task 7

--- supervised_learning/0x08-deep_cnns/7-densenet121.py
+++ supervised_learning/0x08-deep_cnns/7-densenet121.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
-"""DenseNet-121"""
+"""
+    DenseNet-121
 
-
+"""
 import tensorflow.keras as K
 dense_block = __import__('5-dense_block').dense_block
 transition_layer = __import__('6-transition_layer').transition_layer
