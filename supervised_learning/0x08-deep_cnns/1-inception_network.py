58161e6ce78e0c418fff1ac4d26248e3c6f7c6ae
rrrorrigo <rodrigobriones54@gmail.com>
2022-02-10 15:38:53 -0300

    added task 1 and fixed task 0

--- supervised_learning/0x08-deep_cnns/1-inception_network.py
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -15,33 +15,58 @@ def inception_network():
        rectified linear activation (ReLU)
     Returns:    the keras model
     """
-    i = K.layers.Input(shape=(224, 224, 3))
+    init = K.initializers.he_normal()
+    i = K.Input(shape=(224, 224, 3))
 
-    x = K.layers.Conv2D(64, 7, strides=2, padding='same', activation='relu')(i)
-    x = K.layers.MaxPool2D(3, strides=2, padding='same')(x)
+    layer1 = K.layers.Conv2D(filters=64,
+                              kernel_size=7,
+                              padding='same',
+                              strides=2,
+                              kernel_initializer=init,
+                              activation='relu')(i)
+    layer2 = K.layers.MaxPool2D(pool_size=3,
+                                 padding='same',
+                                 strides=2)(layer1)
+    layer3R = K.layers.Conv2D(filters=64,
+                               kernel_size=1,
+                               padding='same',
+                               strides=1,
+                               kernel_initializer=init,
+                               activation='relu')(layer2)
+    layer3 = K.layers.Conv2D(filters=192,
+                              kernel_size=3,
+                              padding='same',
+                              strides=1,
+                              kernel_initializer=init,
+                              activation='relu')(layer3R)
+    layer4 = K.layers.MaxPool2D(pool_size=3,
+                                 padding='same',
+                                 strides=2)(layer3)
+    layer5 = inception_block(layer4, [64, 96, 128, 16, 32, 32])
+    layer6 = inception_block(layer5, [128, 128, 192, 32, 96, 64])
+    layer7 = K.layers.MaxPool2D(pool_size=3,
+                                 padding='same',
+                                 strides=2)(layer6)
+    layer8 = inception_block(layer7, [192, 96, 208, 16, 48, 64])
+    layer9 = inception_block(layer8, [160, 112, 224, 24, 64, 64])
+    layer10 = inception_block(layer9, [128, 128, 256, 24, 64, 64])
+    layer11 = inception_block(layer10, [112, 144, 288, 32, 64, 64])
+    layer12 = inception_block(layer11, [256, 160, 320, 32, 128, 128])
+    layer13 = K.layers.MaxPool2D(pool_size=3,
+                                  padding='same',
+                                  strides=2)(layer12)
+    layer14 = inception_block(layer13, [256, 160, 320, 32, 128, 128])
+    layer15 = inception_block(layer14, [384, 192, 384, 48, 128, 128])
+    layer16 = K.layers.AvgPool2D(pool_size=7,
+                                  padding='same',
+                                  strides=None)(layer15)
 
-    x = K.layers.Conv2D(64, 1, activation='relu')(x)
-    x = K.layers.Conv2D(192, 3, padding='same', activation='relu')(x)
-    x = K.layers.MaxPool2D(3, strides=2, padding='same')(x)
+    layer17 = K.layers.Dropout(0.4)(layer16)
 
-    x = inception_block(x, [64, 96, 128, 16, 32, 32])
-    x = inception_block(x, [128, 128, 192, 32, 96, 64])
-    x = K.layers.MaxPool2D(3, strides=2, padding='same')(x)
+    layer18 = K.layers.Dense(units=1000,
+                              activation='softmax',
+                              kernel_initializer=init,
+                              kernel_regularizer=K.regularizers.l2())(layer17)
 
-    x = inception_block(x, [192, 96, 208, 16, 48, 64])
-    x = inception_block(x, [160, 112, 224, 24, 64, 64])
-    x = inception_block(x, [128, 128, 256, 24, 64, 64])
-    x = inception_block(x, [112, 144, 288, 32, 64, 64])
-    x = inception_block(x, [256, 160, 320, 32, 128, 128])
-    x = K.layers.MaxPool2D(3, strides=2, padding='same')(x)
-
-    x = inception_block(x, [256, 160, 320, 32, 128, 128])
-    x = inception_block(x, [384, 192, 384, 48, 128, 128])
-
-    x = K.layers.AvgPool2D(7, strides=1)(x)
-    x = K.layers.Dropout(0.4)(x)
-
-    output = K.layers.Dense(1000, activation='softmax')(x)
-    model = K.models.Model(i, output)
-
+    model = K.models.Model(inputs=i, outputs=layer18)
     return model
