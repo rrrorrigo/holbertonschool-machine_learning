8853703e32fa3ecefffce5ea2fe47c448cb250af
rrrorrigo <rodrigobriones54@gmail.com>
2022-04-14 20:03:31 -0300

    added advanced task 100

---
+++ math/0x00-linear_algebra/100-slice_like_a_ninja.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Saddle Up"""
+
+
+import numpy as np
+
+
+def np_slice(matrix=[], axes={}):
+    """function that slices a matrix along specific axes"""
+    slc = np.asarray(matrix)
+    newMatrix = []
+    for idx in range(len(slc)):
+        for k, v in axes.items():
+            lenValue = len(v)
+            start = v[0] if lenValue > 0 else None
+            stop = v[1] if lenValue > 1 else None
+            step = v[2] if lenValue > 2 else None
+            axis = k
+            newMatrix.append(slc[idx][start:stop:step])
+    return np.asarray(newMatrix)
