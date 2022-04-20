82538978336d2db42025c361b02464b1b2b2af0b
rrrorrigo <rodrigobriones54@gmail.com>
2022-04-20 19:46:28 -0300

    added task 17

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""Integrate"""
+
+
+def poly_integral(poly, C=0):
+    """Function that calculates the integral of a polynomial
+
+    poly: is a list of coefficients representing a polynomial
+        - the index of the list represents the power of x that the
+          coefficient belongs to
+        - Example: if f(x) = x^3 + 3x +5, poly is equal to [5, 3, 0, 1]
+    C: is an integer representing the integration constant
+
+    Return: a new list of coefficients representing the integral of
+    the polynomial"""
+    # i == power of x
+    result = [0]
+    for i in range(1, len(poly) + 1):
+        x = poly[i - 1] / i
+        if (x - int(x)) == 0:
+            number = int(x)
+        else:
+            number = x
+        result.append(number)
+    return result
