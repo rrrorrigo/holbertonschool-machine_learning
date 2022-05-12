d3f80c0fe951dbdc1019751dd0b905423cb6e892
Rodrigo Briones <rbriones@zonamerica.com>
2022-05-12 17:39:08 -0300

    added task 3

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -8,8 +8,10 @@ import tensorflow.compat.v1 as tf
 def calculate_accuracy(y, y_pred):
     """Function that calculates the accuracy of a prediction
 
-    y: is a placeholder for the labels of the inpu data
+    y: is a placeholder for the labels of the input data
     y_pred: is a tensor containing the network's predictions
 
     Returns: a tensor containing the decimal accuracy of the prediction"""
-    return tf.metrics.accuracy(y, y_pred)[0]
+    equality = tf.math.equal(y_pred, y)
+    acc = tf.math.reduce_mean(tf.cast(equality, tf.float32))
+    return acc
