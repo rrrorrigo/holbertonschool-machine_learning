63e2fffec2b794b0e2f21ba91467639a3ba91e4a
Rodrigo Briones <rbriones@zonamerica.com>
2022-05-12 09:47:43 -0300

    added task 0

---
+++ supervised_learning/0x02-tensorflow/0-create_placeholders.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Create placeholder"""
+
+
+import tensorflow.compat.v1 as tf
+
+
+def create_placeholders(nx, classes):
+    """Function that return two placeholders, x and y, for the neural network
+
+    nx: the number of feature columns in our data
+    classes: the number of classes in our classifier
+
+    Return: placeholders named x and y, respectively
+        x: is the placeholder for the input data to the neural network
+        y: is the placeholder fot the one-hot labels for the input data"""
+    x = tf.placeholder('float32', shape=[nx], name="x")
+    y = tf.placeholder('float32', shape=[classes], name="y")
+    return x, y
