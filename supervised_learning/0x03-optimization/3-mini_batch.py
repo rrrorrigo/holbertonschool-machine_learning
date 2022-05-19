1d1c00cc5b6ea6ec941c82863da3988936d4e495
rrrorrigo <rodrigobriones54@gmail.com>
2022-05-18 23:15:37 -0300

    fixed pycodestyle

--- supervised_learning/0x03-optimization/3-mini_batch.py
+++ supervised_learning/0x03-optimization/3-mini_batch.py
@@ -18,12 +18,16 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
     X_train: is a numpy.ndarray of shape (m, 784) containing the training data
         m is the number of data points
         784 is the number of input features
-    Y_train: is a one-hot numpy.ndarray of shape (m, 10) containing the training labels
+    Y_train: is a one-hot numpy.ndarray of shape (m, 10) containing the
+    training labels
         10 is the number of classes the model should classify
-    X_valid: is a numpy.ndarray of shape (m, 784) containing the validation data
-    Y_valid: is a one-hot numpy.ndarray of shape (m, 10) containing the validation labels
+    X_valid: is a numpy.ndarray of shape (m, 784) containing the validation
+    data
+    Y_valid: is a one-hot numpy.ndarray of shape (m, 10) containing the
+    validation labels
     batch_size: is the number of data points in a batch
-    epochs: is the number of times the training should pass through the whole dataset
+    epochs: is the number of times the training should pass through the whole
+    dataset
     load_path: is the path from which to load the model
     save_path: is the path to where the model should be saved after training
 
@@ -38,7 +42,7 @@ def train_mini_batch(X_train, Y_train, X_valid, Y_valid, batch_size=32,
         train_op = tf.get_collection('train_op')[0]
         feed_train = {x: X_train, y: Y_train}
         feed_valid = {x: X_valid, y: Y_valid}
-        
+
         batch_float = X_train.shape[0] / batch_size
         batch_int = int(batch_float)
 
