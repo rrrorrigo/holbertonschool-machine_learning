4d582bba777ac422a3e74c870a484a4f045f404b
ZONAMERICA\rbriones <rbriones@zonamerica.com>
2022-05-26 11:18:51 -0300

    added task 1

--- supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
+++ supervised_learning/0x05-regularization/1-l2_reg_gradient_descent.py
@@ -19,18 +19,17 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
     lambtha: is the L2 regularization parameter
     L: is the number of layers of the network"""
     m = Y.shape[1]
-    weight = weights.copy()
     la = L
     a = 'A' + str(la)
     W = 'W' + str(la)
     b = 'b' + str(la)
     dz = cache[a] - Y
     dw = (np.dot(cache['A' + str(la - 1)], dz.T) / m).T
-    dw = dw + (lambtha / m) * weight[W]
+    dw = dw + (lambtha / m) * weights[W]
     db = np.sum(dz, axis=1, keepdims=True) / m
 
-    weights[W] = weight[W] - alpha * dw
-    weights[b] = weight[b] - alpha * db
+    weights[W] = weights[W] - alpha * dw
+    weights[b] = weights[b] - alpha * db
 
     for la in range(L - 1, 0, -1):
         a = 'A' + str(la)
@@ -39,9 +38,9 @@ def l2_reg_gradient_descent(Y, weights, cache, alpha, lambtha, L):
         wNext = 'W' + str(la + 1)
         aNext = 'A' + str(la - 1)
         g = cache[a] * (1 - cache[a])
-        dz = np.dot(weight[wNext].T, dz) * g
-        dw = (np.dot(cache[aNext], dz.T) / m).T + ((lambtha / m) * weight[W])
+        dz = np.dot(weights[wNext].T, dz) * g
+        dw = (np.dot(cache[aNext], dz.T) / m).T + ((lambtha / m) * weights[W])
         db = np.sum(dz, axis=1, keepdims=True) / m
 
-        weights[W] = weight[W] - alpha * dw
-        weights[b] = weight[b] - alpha * db
+        weights[W] = weights[W] - alpha * dw
+        weights[b] = weights[b] - alpha * db
