4382d2002a16219347baf9c675538441767ad05a
rrrorrigo <rodrigobriones54@gmail.com>
2022-05-26 15:37:59 -0300

    added task 2 and its main file

---
+++ supervised_learning/0x05-regularization/2-l2_reg_cost.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""L2 regularization cost with tensorflow"""
+
+
+import tensorflow.compat.v1 as tf
+
+
+def l2_reg_cost(cost):
+    """Function that calculates the cost of a neural network with L2
+    regularization:
+
+    cost: is a tensor containing the cost of the network without L2
+    regularization
+
+    Returns: a tensor containing the cost of the network accounting for L2
+    regularization"""
+    return cost + tf.losses.get_regularization_losses()
