f9fbdbf91f6f173b7ce8db22d73834959543c9b5
rrrorrigo <rodrigobriones54@gmail.com>
2022-05-26 15:49:57 -0300

    added task 3 and its main file

---
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Create a Layer with regularization L2"""
+
+
+import tensorflow.compat.v1 as tf
+
+
+def l2_reg_create_layer(prev, n, activation, lambtha):
+    """Function that creates a tensorflow layer that includes L2
+    regularization:
+
+    prev: is a tensor containing the output of the previous layer
+    n: is the number of nodes the new layer should contain
+    activation: is the activation function that should be used on the layer
+    lambtha: is the L2 regularization parameter
+
+    Returns: the output of the new layer"""
+    init = tf.keras.initializers.VarianceScaling(scale=2.0, mode=("fan_avg"))
+    l2 = tf.keras.regularizers.L2(lambtha)
+    new_layer = tf.layers.Dense(n, activation=activation,
+                                kernel_initializer=init,
+                                kernel_regularizer=l2)(prev)
+    return new_layer
