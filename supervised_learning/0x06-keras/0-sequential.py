3c5eb1350f5cbfe5bae35e4b7d705a375883e6e2
rrrorrigo <rodrigobriones54@gmail.com>
2022-05-31 16:02:19 -0300

    added task 0 and its main file

--- supervised_learning/0x06-keras/0-sequential.py
+++ supervised_learning/0x06-keras/0-sequential.py
@@ -23,6 +23,6 @@ def build_model(nx, layers, activations, lambtha, keep_prob):
         model.add(K.layers.Dense(layers[i], activation=activations[i],
                                  input_shape=(nx,),
                                  kernel_regularizer=regularizer))
-        if i < len(layers):
+        if i < len(layers) - 1:
             model.add(K.layers.Dropout(1 - keep_prob))
     return model
