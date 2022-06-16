85631fa48a53d97aba8e9963ab0a477fb3b393ab
ZONAMERICA\rbriones <rbriones@zonamerica.com>
2022-06-16 10:42:14 -0300

    added task 1

---
+++ supervised_learning/0x08-deep_cnns/1-inception_network.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+"""Inception Network"""
+
+
+import tensorflow.keras as K
+inception_block = __import__('0-inception_block').inception_block
+
+
+def inception_network():
+    """that builds the inception network as described in Going Deeper with
+    Convolutions (2014):
+
+    the input data will have shape (224, 224, 3)
+    All convolutions inside and outside the inception block use a
+    rectified linear activation (ReLU)
+
+    Returns: the keras model"""
+    x = K.Input(shape=(224, 224, 3))
+    layer1 = K.layers.Conv2D(64, (7, 7), strides=(2, 2), padding='same',
+                             activation='relu')(x)
+    layer2 = K.layers.MaxPooling2D((3, 3), strides=(2, 2))(layer1)
+    layer3 = K.layers.Conv2D(64, (3, 3), strides=(1, 1), padding='same',
+                             activation='relu')(layer2)
+    layer4 = K.layers.Conv2D(192, (3, 3), strides=(1, 1), padding='same',
+                             activation='relu')(layer3)
+    layer4 = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(layer4)
+
+    # Inception 3a
+    Y = [64, 96, 128, 16, 32, 32]
+    layer5 = inception_block(layer4, Y)
+    layer6 = inception_block(layer5, Y)
+
+    # Inception 3b
+    Y = [128, 128, 192, 32, 96, 64]
+    layer7 = inception_block(layer6, Y)
+    layer8 = inception_block(layer7, Y)
+
+    layer9 = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(layer8)
+
+    # Inception 4a
+    Y = [192, 96, 208, 16, 48, 64]
+    layer10 = inception_block(layer9, Y)
+    layer11 = inception_block(layer10, Y)
+
+    # Inception 4b
+    Y = [160, 112, 224, 24, 64, 64]
+    layer12 = inception_block(layer11, Y)
+    layer13 = inception_block(layer12, Y)
+
+    # Inception 4c
+    Y = [128, 128, 256, 24, 64, 64]
+    layer14 = inception_block(layer13, Y)
+    layer15 = inception_block(layer14, Y)
+
+    # Inception 4d
+    Y = [112, 144, 288, 32, 64, 64]
+    layer16 = inception_block(layer15, Y)
+    layer17 = inception_block(layer16, Y)
+
+    # Inception 4e
+    Y = [256, 160, 320, 32, 128, 128]
+    layer18 = inception_block(layer17, Y)
+    layer19 = inception_block(layer18, Y)
+
+    layer20 = K.layers.MaxPooling2D((3, 3), strides=(2, 2), padding='same')(layer19)
+
+    # Inception 5a
+    Y = [256, 160, 320, 32, 128, 128]
+    layer21 = inception_block(layer20, Y)
+    layer22 = inception_block(layer21, Y)
+
+    # Inception 5b
+    Y = [384, 192, 384, 48, 128, 128]
+    layer23 = inception_block(layer22, Y)
+    layer24 = inception_block(layer23, Y)
+
+    layer25 = K.layers.AveragePooling2D((7, 7), strides=1)(layer24)
+
+    layer26 = K.layers.Dropout(.40)(layer25)
+
+    linear = K.activations.linear(layer26)
+    output = K.layers.Dense(1000, activation='softmax')(linear)
+
+    model = K.Model(x, output)
+
+    return model
