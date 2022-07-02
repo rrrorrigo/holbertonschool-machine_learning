c7231f022e80c24aa29643d19230d3195024de5f
rrrorrigo <rodrigobriones54@gmail.com>
2022-07-02 10:57:42 -0300

    fixed pycodestyle

--- supervised_learning/0x0A-object_detection/0-yolo.py
+++ supervised_learning/0x0A-object_detection/0-yolo.py
@@ -14,14 +14,16 @@ class Yolo:
         model_path: is the path to where a Darknet Keras model is stored
         classes_path: is the path to where the list of class names used for the
         Darknet model, listed in order of index, can be found
-        class_t: is a float representing the box score threshold for the initial
-        filtering step
-        nms_t: is a float representing the IOU threshold for non-max suppression
-        anchors: is a numpy.ndarray of shape (outputs, anchor_boxes, 2) containing
-        all of the anchor boxes:
-            outputs: is the number of outputs (predictions) made by the Darknet model
-            anchor_boxes: is the number of anchor boxes used for each prediction
-            2 => [anchor_box_width, anchor_box_height]"""
+        class_t: is a float representing the box score threshold for the
+        initial filtering step
+        nms_t: is a float representing the IOU threshold for non-max
+        suppression
+        anchors: is a numpy.ndarray of shape (outputs, anchor_boxes, 2)
+        containing all of the anchor boxes:
+            outputs: is the number of outputs (predictions) made by the Darknet
+            model
+            anchor_boxes: is the number of anchor boxes used for each
+            prediction 2 => [anchor_box_width, anchor_box_height]"""
         self.model = K.models.load_model(model_path)
         with open(classes_path, 'r') as f:
             self.class_names = f.readlines()
