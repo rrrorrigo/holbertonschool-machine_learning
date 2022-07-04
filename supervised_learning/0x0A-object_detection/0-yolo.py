debc95f251499922d165fcb0a0b11da16d80f851
rrrorrigo <rodrigobriones54@gmail.com>
2022-07-04 09:29:29 -0300

    removed new line when load classes_path

--- supervised_learning/0x0A-object_detection/0-yolo.py
+++ supervised_learning/0x0A-object_detection/0-yolo.py
@@ -26,7 +26,10 @@ class Yolo:
             prediction 2 => [anchor_box_width, anchor_box_height]"""
         self.model = K.models.load_model(model_path)
         with open(classes_path, 'r') as f:
-            self.class_names = f.readlines()
+            txt = f.read().split('\n')
+            if len(txt[-1]) == 0:
+                txt = txt[:-1]
+        self.class_names = txt
         self.class_t = class_t
         self.nms_t = nms_t
         self.anchors = anchors
