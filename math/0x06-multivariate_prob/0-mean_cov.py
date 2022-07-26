6d04b73daf2cef43b4a51b0f28cc99345f5fef8a
rrrorrigo <rodrigobriones54@gmail.com>
2022-07-25 23:13:36 -0300

    added task 0

---
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+"""mean covariance"""
+
+
+import numpy as np
+
+
+def mean_cov(X):
+    """Function that calculate the covariance of dataset
+
+    X is a numpy.ndarray of shape (n, d) containing the data set:
+        n is the number of data points
+        d is the number of dimensions in each data point
+    If X is not a 2D numpy.ndarray, raise a TypeError with the message X
+    must be a 2D numpy.ndarray
+    If n is less than 2, raise a ValueError with the message X must contain
+    multiple data points
+    Returns: mean, cov:
+        mean is a numpy.ndarray of shape (1, d) containing the mean of the
+        data set
+        cov is a numpy.ndarray of shape (d, d) containing the covariance matrix
+        of the data set"""
+    if len(X.shape) != 2:
+        raise TypeError('X must be a 2D numpy.ndarray')
+    if len(X[0]) < 2:
+        raise ValueError('X must contain multiple data points')
+    data, dim = X.shape
+    mean = np.mean(X[:], axis=0)
+    x = X - mean
+    cov = (x.T @ x) / (data - 1)
+    return mean, cov
