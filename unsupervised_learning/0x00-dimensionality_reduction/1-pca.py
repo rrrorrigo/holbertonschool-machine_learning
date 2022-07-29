8dcc0c1d7b1ee32885e87dd720f1606ef6284c54
rrrorrigo <rodrigobriones54@gmail.com>
2022-07-28 22:42:16 -0300

    added task 1

--- unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
+++ unsupervised_learning/0x00-dimensionality_reduction/1-pca.py
@@ -14,20 +14,10 @@ def pca(X, ndim):
     ndim is the new dimensionality of the transformed X
     Returns: T, a numpy.ndarray of shape (n, ndim) containing the transformed
     version of X"""
-    X_meaned = X - np.mean(X , axis = 0)
-     
-    cov_mat = np.cov(X_meaned , rowvar = False)
-     
-    eigen_values , eigen_vectors = np.linalg.eigh(cov_mat)
-     
-    # arranging them in descending order of their Eigenvalue will automatically
-    # arrange the principal component in descending order of their variability
-    sorted_index = np.argsort(eigen_values)[::-1]
-    sorted_eigenvalue = eigen_values[sorted_index]
-    sorted_eigenvectors = eigen_vectors[:,sorted_index]
-     
-    eigenvector_subset = sorted_eigenvectors[:,0:ndim]
-     
-    X_reduced = np.dot(eigenvector_subset.transpose() , X_meaned.transpose() ).transpose()
-     
-    return X_reduced
+    X_meaned = X - np.mean(X, axis=0)
+
+    lsv, sigma, rsv = np.linalg.svd(X_meaned)
+
+    pca_matrix = np.matmul(lsv[..., :ndim], np.diag(sigma[..., :ndim]))
+
+    return pca_matrix
