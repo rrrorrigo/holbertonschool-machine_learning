87a57d59b5bc18ba7d2388e726146c91c59f084f
rrrorrigo <rodrigobriones54@gmail.com>
2022-08-03 18:25:21 -0300

    added task 3

--- unsupervised_learning/0x01-clustering/3-optimum.py
+++ unsupervised_learning/0x01-clustering/3-optimum.py
@@ -22,7 +22,22 @@ def optimum_k(X, kmin=1, kmax=None, iterations=1000):
         cluster size
         d_vars is a list containing the difference in variance from the
         smallest cluster size for each cluster size"""
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None, None
+    if type(iterations) is not int or iterations <= 0:
+        return None, None
+    if type(kmin) is not int or kmin < 1:
+        return None, None
+    if kmax is not None and (type(kmax) is not int or kmax < 1):
+        return None, None
+    if kmax is not None and kmin >= kmax:
+        return None, None
+
     n, d = X.shape
+
+    if kmax is None:
+        kmax = n
+
     results, d_vars = [], []
     for k in range(kmin, kmax + 1):
         C, closest_i = kmeans(X, k)
