4afe7bc626012076262effb3718f5947a3df5f51
rrrorrigo <rodrigobriones54@gmail.com>
2022-08-03 16:37:31 -0300

    added task 2

--- unsupervised_learning/0x01-clustering/2-variance.py
+++ unsupervised_learning/0x01-clustering/2-variance.py
@@ -14,6 +14,10 @@ def variance(X, C):
     each cluster
     Returns: var, or None on failure
     var is the total variance"""
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None
+    if type(C) is not np.ndarray or len(C.shape) != 2:
+        return None
     distance = np.sqrt(np.sum((X - C[:, np.newaxis]) ** 2, axis=2))
     var = np.sum(np.sum(distance.min(axis=0)**2))
 
