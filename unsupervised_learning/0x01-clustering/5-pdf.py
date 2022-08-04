d32faaa6e22c4435773604e6f78e752b18b57c53
rrrorrigo <rodrigobriones54@gmail.com>
2022-08-04 20:01:10 -0300

    added task 5

--- unsupervised_learning/0x01-clustering/5-pdf.py
+++ unsupervised_learning/0x01-clustering/5-pdf.py
@@ -18,6 +18,16 @@ def pdf(X, m, S):
     Returns: P, or None on failure
         P is a numpy.ndarray of shape (n,) containing the PDF values for
         each data point"""
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None
+    if type(m) is not np.ndarray or len(m.shape) != 1:
+        return None
+    if type(S) is not np.ndarray or len(S.shape) != 2:
+        return None
+    if X.shape[1] != m.shape[0] or X.shape[1] != S.shape[0]:
+        return None
+    if S.shape[0] != S.shape[1]:
+        return None
     n, d = X.shape
     f = 1 / np.sqrt(((2*np.pi)**d) * np.linalg.det(S))
     Xm = (X - m).T
