4853cc47110be6f0e5b6036d9a4d14c409cc78d0
rrrorrigo <rodrigobriones54@gmail.com>
2022-08-04 21:20:25 -0300

    added task 7

--- unsupervised_learning/0x01-clustering/7-maximization.py
+++ unsupervised_learning/0x01-clustering/7-maximization.py
@@ -20,6 +20,16 @@ def maximization(X, g):
         centroid means for each cluster
         S is a numpy.ndarray of shape (k, d, d) containing the updated
         covariance matrices for each cluster"""
+    if type(X) is not np.ndarray or len(X.shape) != 2:
+        return None, None, None
+    if type(g) is not np.ndarray or len(g.shape) != 2:
+        return None, None, None
+    if X.shape[0] != g.shape[1]:
+        return None, None, None
+    cluster = np.sum(g, axis=0)
+    cluster = np.sum(cluster)
+    if int(cluster) != X.shape[0]:
+        return None, None, None
     n, d = X.shape
     k = g.shape[0]
 
@@ -29,10 +39,9 @@ def maximization(X, g):
     m = np.zeros((k, d))
     S = np.zeros((k, d, d))
 
-
     for i in range(k):
         m[i] = np.matmul(g[i], X) / responsabilities[i]
         x_meaned = X - m[i]
         S[i] = np.matmul(g[i] * x_meaned.T, x_meaned) / responsabilities[i]
-    
+
     return pi, m, S
