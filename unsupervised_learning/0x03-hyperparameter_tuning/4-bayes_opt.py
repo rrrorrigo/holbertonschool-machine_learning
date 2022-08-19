3a905a63d83ceae442653b0647f7b668de24cfb6
rrrorrigo <rodrigobriones54@gmail.com>
2022-08-18 22:38:52 -0300

    added task 4

--- unsupervised_learning/0x03-hyperparameter_tuning/4-bayes_opt.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/4-bayes_opt.py
@@ -34,7 +34,7 @@ class BayesianOptimization:
         self.f = f
         self.gp = GP(X_init, Y_init, l, sigma_f)
         self.X_s = np.linspace(bounds[0], bounds[1],
-                               ac_samples).reshape(-1, 1)
+                               ac_samples).reshape(ac_samples, 1)
         self.xsi = xsi
         self.minimize = minimize
 
@@ -48,22 +48,14 @@ class BayesianOptimization:
             the expected improvement of each potential sample"""
         mu, sigma = self.gp.predict(self.X_s)
 
-        if self.minimize is False:
-            mu_sample_opt = np.amax(self.gp.Y)
-            imp = mu - mu_sample_opt - self.xsi
-        else:
-            mu_sample_opt = np.amin(self.gp.Y)
-            imp = mu_sample_opt - mu - self.xsi
+        sample = np.amin(self.gp.Y) if self.minimize else np.amax(self.gp.Y)
 
-        Z = np.zeros(sigma.shape)
-        for i in range(len(sigma)):
-            if sigma[i] != 0:
-                Z[i] = imp[i] / sigma[i]
-            else:
-                Z[i] = 0
-        ei = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
-        ei[sigma == 0.0] = 0.0
+        # class that avoid floating point number error
+        with np.errstate(divide='warn'):
+            imp = sample - mu if self.minimize else mu - sample - self.xsi
+            Z = imp / sigma
+            EI = imp * norm.cdf(Z) + sigma * norm.pdf(Z)
+            EI[sigma == 0.0] = 0.0
 
-        X_next = self.X_s[np.argmax(ei)]
-
-        return X_next, ei
+        X_next = self.X_s[np.argmax(EI)]
+        return X_next, EI
