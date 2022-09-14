e9b817dac97236eae07f4d68f5767a9ed80a0207
rrrorrigo <rodrigobriones54@gmail.com>
2022-09-13 21:24:20 -0300

    added task 0

---
+++ supervised_learning/0x0F-word_embeddings/0-bag_of_words.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+"""Bag of words"""
+
+
+from sklearn.feature_extraction.text import CountVectorizer
+
+
+def bag_of_words(sentences, vocab=None):
+    """Function  that creates a bag of words embedding matrix:
+
+    sentences is a list of sentences to analyze
+    vocab is a list of the vocabulary words to use for the analysis
+        If None, all words within sentences should be used
+    Returns: embeddings, features
+        embeddings is a numpy.ndarray of shape (s, f) containing the embeddings
+            s is the number of sentences in sentences
+            f is the number of features analyzed
+        features is a list of the features used for embeddings"""
+    s_vector = CountVectorizer(vocabulary=vocab)
+    embeddings = s_vector.fit_transform(sentences)
+    embeddings = embeddings.toarray()
+    features = s_vector.get_feature_names()
+    return embeddings, features
