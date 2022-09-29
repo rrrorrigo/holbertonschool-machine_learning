ad3c5104139d99e37f9aee3cfd25176857d08d0e
ZONAMERICA\rbriones <rbriones@zonamerica.com>
2022-09-29 15:48:03 -0300

    added task 0

---
+++ supervised_learning/0x12-transformer_apps/0-dataset.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python3
+"""Dataset"""
+
+
+import tensorflow_datasets as tfds
+
+
+class Dataset:
+    """Dataset class that loads and preps a dataset for machine translation"""
+
+    def __init__(self):
+        """Class constructor
+
+        creates the instance attributes:
+        data_train, which contains the ted_hrlr_translate/pt_to_en
+         tf.data.Dataset train split, loaded as_supervided
+        data_valid, which contains the ted_hrlr_translate/pt_to_en
+         tf.data.Dataset validate split, loaded as_supervided
+        tokenizer_pt is the Portuguese tokenizer created from the training set
+        tokenizer_en is the English tokenizer created from the training set"""
+        self.data_train = tfds.load('ted_hrlr_translate/pt_to_en',
+                                    split='train', as_supervised=True)
+        self.data_valid = tfds.load('ted_hrlr_translate/pt_to_en',
+                                    split='validation', as_supervised=True)
+        self.tokenizer_pt, self.tokenizer_en = self.tokenize_dataset(self.data_train)
+
+    def tokenize_dataset(self, data):
+        """Function that creates sub-word tokenizers for the dataset
+
+        data is a tf.data.Dataset whose examples are formatted
+        as a tuple (pt, en)
+        pt is the tf.Tensor containing the Portuguese sentence
+        en is the tf.Tensor containing the corresponding English sentence
+
+        The maximum vocab size should be set to 2**15
+        Returns: tokenizer_pt, tokenizer_en
+            tokenizer_pt is the Portuguese tokenizer
+            tokenizer_en is the English tokenizer"""
+        tokenizer_pt = tfds.deprecated.text\
+                       .SubwordTextEncoder\
+                       .build_from_corpus(
+                        (pt.numpy() for pt, en in data), 2**15)
+        tokenizer_en = tfds.deprecated.text\
+                       .SubwordTextEncoder\
+                       .build_from_corpus(
+                        (en.numpy() for pt, en in data), 2**15)
+        return tokenizer_pt, tokenizer_en
